c54a18d6f7aa27ed1c5712b8f59dd8265e5034e4
dbaigalmaa <dbaigalmaa@gmail.com>
2022-10-24 12:14:27 -0700

    Showing icon

--- src/app.js
+++ src/app.js
@@ -1,16 +1,46 @@
+function formatDate(timestamp) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDate = new Date(timestamp * 1000);
+  let day = days[currentDate.getDay()];
+  let hour = currentDate.getHours();
+  let minute = currentDate.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  return `${day} ${hour}:${minute}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let weatherDesc = document.querySelector("#weather-desc");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let humidityElement = document.querySelector("#humidity");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   weatherDesc.innerHTML = response.data.weather[0].main;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  console.log(response.data);
-
-  let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
+  dateElement.innerHTML = formatDate(response.data.dt);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  console.log(response.data.weather[0].icon);
 }
 
 let apiKey = "7f74d00955f9d04bcd04016edb71bf36";
