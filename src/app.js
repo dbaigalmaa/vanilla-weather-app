ec7a5ae2ec6864dad5b4c3933738936af604c964
dbaigalmaa <dbaigalmaa@gmail.com>
2022-10-24 16:37:34 -0700

    added forecast JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,38 @@ function formatDate(timestamp) {
   return `${day} ${hour}:${minute}`;
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = [0, 1, 2, 4, 5];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    let tempMax = Math.round(response.data.daily[day].temperature.maximum);
+    let tempMin = Math.round(response.data.daily[day].temperature.minimum);
+    let iconUrl = response.data.daily[day].condition.icon_url;
+    let iconAlt = response.data.daily[day].condition.icon;
+    let dayForecast = formatDate(
+      response.data.daily[day].time * 1000
+    ).substring(0, 3);
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="forecast-date">${dayForecast}</div>
+              <img
+                src=${iconUrl}
+                alt=${iconAlt}
+                width="46"
+              />
+              <div class="forecast-temp">
+                <span class="forectas-temp-max">${tempMax}°</span>
+                <span class="forecast-temp-min">${tempMin}°</span>
+              </div>
+            </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHTML + `</div>`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -45,7 +77,9 @@ function displayTemperature(response) {
 function search(city) {
   let apiKey = "acab64483d142d2af0b5de09tad9fo2f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiForecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiForecastUrl).then(displayForecast);
 }
 function handleSubmit(event) {
   event.preventDefault();
